6380e6af3a4031e274c4cc077121bce6736e9017
shridhar5me <5minutesengineering@gmail.com>
2025-10-18 10:24:27 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import re
-def extract_email(text):
-    m = re.search(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", text)
-    return m.group(0) if m else ""
-
-def extract_phone(text):
-    m = re.search(r"(\+?\d[\d\-\s]{7,}\d)", text)
-    return m.group(0) if m else ""
